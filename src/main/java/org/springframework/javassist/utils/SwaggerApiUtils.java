52c69f6a9ad77292b2376c14057ad4fb8331c61b
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-18 11:00:46 +0800

    refactor: groupId io.github.hiwepy -> io.github.easy4j, package rename

--- src/main/java/org/springframework/javassist/utils/SwaggerApiUtils.java
+++ src/main/java/org/springframework/javassist/utils/SwaggerApiUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -20,7 +20,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.javassist.bytecode.definition.MvcApiImplicitParam;
 import org.springframework.javassist.bytecode.definition.MvcApiResponse;
 
-import io.github.hiwepy.javassist.bytecode.CtAnnotationBuilder;
+import io.github.easy4j.javassist.bytecode.CtAnnotationBuilder;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -35,7 +35,7 @@ import javassist.bytecode.annotation.Annotation;
 
 /**
  * https://www.cnblogs.com/sunfie/p/5154246.html
- * @author <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class SwaggerApiUtils {
 
